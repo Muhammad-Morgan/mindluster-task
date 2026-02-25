d73a2a98396a363cfeea36eb951bb015add71a01
Muhammad Morgan <muhammadmorgan589@gmail.com>
2026-02-25 21:46:47 +0200

    add lucide-icons for grid icon and setup the nav links

---
+++ utils/navLinks.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { CheckSquare, Edit } from "lucide-react";
+
+export const navLinks = [
+  { href: "/tasks", label: "Tasks", icon: CheckSquare },
+  { href: "/tasks/edit-task", label: "Edit Task", icon: Edit },
+];
